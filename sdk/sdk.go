59bd33d44fbc9ac6a4ba3bd87d9f087fef73524f
milin2436 <canyelengxin@gmail.com>
2024-05-29 14:00:57 +0800

    sdk api

---
+++ sdk/sdk.go
@@ -0,0 +1,38 @@
+package sdk
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/milin2436/BaiduPCS-Go/internal/pcscommand"
+	"github.com/milin2436/BaiduPCS-Go/internal/pcsconfig"
+	"github.com/milin2436/BaiduPCS-Go/pcsutil"
+)
+
+func SdkInit() {
+	pcsutil.ChWorkDir()
+	err := pcsconfig.Config.Init()
+	switch err {
+	case nil:
+	case pcsconfig.ErrConfigFileNoPermission, pcsconfig.ErrConfigContentsParseError:
+		fmt.Fprintf(os.Stderr, "FATAL ERROR: config file error: %s\n", err)
+		os.Exit(1)
+	default:
+		fmt.Printf("WARNING: config init error: %s\n", err)
+	}
+}
+
+func SdkClose() {
+	defer pcsconfig.Config.Close()
+	q, u, err := pcscommand.GetBaiduPCS().QuotaInfo()
+
+	if err != nil {
+		fmt.Println("err = ", err)
+		return
+	}
+	fmt.Printf("%d\n", q)
+	fmt.Printf("%d\n", u)
+}
+func RunDownload(paths []string) {
+	pcscommand.RunDownload(paths, nil)
+}
